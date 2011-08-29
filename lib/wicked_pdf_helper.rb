239c913d7f209142a0fe6b3f29038d2bcd59a626
zaiste <oh@zaiste.net>
2011-08-29 12:26:58 +0200

    Rails 3.1 images path

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -7,7 +7,7 @@ module WickedPdfHelper
   end
 
   def wicked_pdf_image_tag(img, options={})
-    image_tag "file://#{Rails.root.join('public', 'images', img)}", options
+    image_tag "file://#{Rails.root.join('app', 'assets', 'images', img)}", options
   end
 
   def wicked_pdf_javascript_src_tag(jsfile, options={})
