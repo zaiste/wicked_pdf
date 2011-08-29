bc3219fac616d9bc4ada4c09c8aa279df6198649
zaiste <oh@zaiste.net>
2011-08-29 10:04:26 +0200

    Rails 3 compatible

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -1,6 +1,6 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(*sources)
-    css_dir = Rails.root.join('public','stylesheets')
+    css_dir = Rails.root.join('app', 'assets', 'stylesheets')
     sources.collect { |source|
       "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
     }.join("\n").html_safe
